176ef82f3ecbbd06a673f137057cb45dc5a70026
Alan James Américo <alanjamesamerico@gmail.com>
2016-12-28 17:58:54 -0300

    [ADD][MOD] - Quartz reconfigurado. Class JLexService alterada

--- src/main/java/br/com/jlex/bot/services/JLexService.java
+++ src/main/java/br/com/jlex/bot/services/JLexService.java
@@ -1,53 +1,16 @@
 package br.com.jlex.bot.services;
 
-import java.io.Serializable;
-
 import org.quartz.DisallowConcurrentExecution;
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
-import org.springframework.scheduling.quartz.QuartzJobBean;
 import org.springframework.stereotype.Component;
-
-import br.com.jlex.bot.core.QuartzTest;
 
 @Component("jlexService")
 @DisallowConcurrentExecution
-public class JLexService extends QuartzJobBean implements Serializable {
+public class JLexService extends JLexServiceAbstract {
+
+	private static final long serialVersionUID = -6143356813365910975L;
 	
-	private static final long serialVersionUID = 2282281468243216898L;
+	@SuppressWarnings("unused")
+	private String telegramAPi;
 	
-	private String telegramApi;
-	
-	@Override
-	protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
-		try {
-			synchronized (Thread.currentThread()) {
-				System.out.println("[Thread] Name  - " + Thread.currentThread().getName());
-				System.out.println("[Thread] State - " + Thread.currentThread().getState());
-				
-				QuartzTest.test();
-				
-				System.out.println("# FIM TEST #");
-			}
-		} catch (Throwable  t) {
-			t.printStackTrace();
-		}
-	}
-	
-	//@Scheduled(fixedDelay=5000)
-	public void initGetMessagesSenders() {
-		try {
-			synchronized (this) {
-				System.out.println("[Thread] Name  - " + Thread.currentThread().getName());
-				System.out.println("[Thread] State - " + Thread.currentThread().getState());
-				
-				QuartzTest.test();
-				
-				System.out.println("# FIM TEST #");
-			}
-		} catch (Throwable  t) {
-			System.err.println("\t* ERROR INIT *");
-			t.printStackTrace();
-		}
-	}
+
 }
---
+++ src/main/java/br/com/jlex/bot/services/JLexServiceAbstract.java
@@ -0,0 +1,49 @@
+package br.com.jlex.bot.services;
+
+import java.io.Serializable;
+
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+import br.com.jlex.bot.core.QuartzTest;
+
+@DisallowConcurrentExecution
+public abstract class JLexServiceAbstract extends QuartzJobBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+		try {
+			synchronized (Thread.currentThread()) {
+				System.out.println("[Thread] Name  - " + Thread.currentThread().getName());
+				System.out.println("[Thread] State - " + Thread.currentThread().getState());
+				
+				QuartzTest.test();
+				
+				System.out.println("# FIM TEST #");
+			}
+		} catch (Throwable  t) {
+			t.printStackTrace();
+		}
+	}
+	
+	//@Scheduled(fixedDelay=5000)
+	public void initGetMessagesSenders() {
+		try {
+			synchronized (this) {
+				System.out.println("[Thread] Name  - " + Thread.currentThread().getName());
+				System.out.println("[Thread] State - " + Thread.currentThread().getState());
+				
+				QuartzTest.test();
+				
+				System.out.println("# FIM TEST #");
+			}
+		} catch (Throwable  t) {
+			System.err.println("\t* ERROR INIT *");
+			t.printStackTrace();
+		}
+	}
+}
